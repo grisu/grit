22859e53e1a3c11c37df757528fc52ada3b92aba
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-11-02 15:01:14 +1300

    Refactored test-starter to be used from within scripts...

--- src/main/groovy/grisu/tests/TestStarter.groovy
+++ src/main/groovy/grisu/tests/TestStarter.groovy
@@ -4,43 +4,68 @@ import grisu.frontend.control.login.LoginManager
 import grisu.frontend.view.cli.CliHelpers
 import grisu.tests.testRuns.TestRunFactory
 
-static void main(args) {
+public class TestStarter {
 
-	CliHelpers.enableProgressDisplay(false)
+	final List testruns = []
+	final def si
+	final def backend
 
-	def cli = new CliBuilder(usage: 'grit -b [backend] testfiles')
+	public TestStarter(String backend, List configFiles) {
 
-	cli.h(longOpt:'help', "Show usage information and quit")
-	cli.b(longOpt:'backend', argName:'backend', args:1, required:true, 'Grisu backend to connect to, required')
+		for (file in configFiles) {
+			def trs = TestRunFactory.createTestRuns(file)
+			testruns.addAll(trs)
+		}
 
-	def opt = cli.parse(args)
+		this.backend = backend
+		si = LoginManager.loginCommandline(backend)
+	}
 
 
-	if (opt.h) {
-		cli.usage()
+	public void start() {
+
+		for ( tr in testruns ) {
+			tr.setServiceInterfaces([si])
+			tr.kickOffAndWaitForTestsToFinish()
+			tr.waitForCleanUp()
+			tr.printResults()
+		}
+	}
+
+	public List getTestRuns() {
+		return testruns
+	}
+
+
+	static void main(args) {
+
+
+		CliHelpers.enableProgressDisplay(false)
+
+		def cli = new CliBuilder(usage: 'grit -b [backend] testfiles')
+
+		cli.h(longOpt:'help', "Show usage information and quit")
+		cli.b(longOpt:'backend', argName:'backend', args:1, required:true, 'Grisu backend to connect to, required')
+
+		def opt = cli.parse(args)
+
+
+		if (opt.h) {
+			cli.usage()
+			System.exit(0)
+		}
+
+		def backend = opt.b
+
+
+		TestStarter ts = new TestStarter(backend, opt.arguments())
+
+		ts.start()
+
+
+
+
 		System.exit(0)
 	}
-
-	def backend = opt.b
-
-
-	def testruns = []
-
-	for (file in opt.arguments()) {
-		def trs = TestRunFactory.createTestRuns(file)
-		testruns.addAll(trs)
-	}
-
-	def si = LoginManager.loginCommandline(backend)
-
-
-	for ( tr in testruns ) {
-		tr.setServiceInterfaces([si])
-		tr.kickOffAndWaitForTestsToFinish()
-		tr.waitForCleanUp()
-		tr.printResults()
-	}
-
-	System.exit(0)
 }
 
