7aab34f6a9ac1c7fbfda156c3bdde8495e53cf61
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-12-09 11:41:58 +1300

    Added other login methods for credential config.

--- src/main/groovy/grisu/tests/util/CredentialLoader.groovy
+++ src/main/groovy/grisu/tests/util/CredentialLoader.groovy
@@ -1,6 +1,10 @@
 package grisu.tests.util
 
+import grisu.jcommons.utils.MyProxyServerParams;
 import grith.jgrith.Credential
+import grith.jgrith.CredentialFactory;
+import grith.jgrith.plainProxy.LocalProxy;
+import grith.jgrith.utils.CliLogin
 
 
 class CredentialLoader {
@@ -19,12 +23,69 @@ class CredentialLoader {
 					Credential c = loadLocal(config)
 					credentials.put(name, c)
 					break
+				case Credential.Type.SLCS:
+					Credential c = createSlcs(config)
+					credentials.put(name, c)
+					break
+				case Credential.Type.MyProxy:
+					Credential c = loadMyProxy(config)
+					credentials.put(name, c)
+					break
+				case Credential.Type.Proxy:
+					Credential c = loadLocalProxy(config)
+					credentials.put(name, c)
+					break
 				default: 
 					print 'default'
 			}
 		}
 		return credentials
 		
+	}
+	
+	static Credential loadLocalProxy(ConfigObject co) {
+		def path = co.get('path')
+		if ( ! path ) {
+			path = LocalProxy.PROXY_FILE
+		}
+		Credential c = new Credential(path)
+		return c
+	}
+	
+	static Credential loadMyProxy(ConfigObject co) {
+		
+		def username = co.get('username')
+		def password = co.get('password')
+		def myproxy = co.get('host')
+		def port = co.get('port')
+		def lifetime = co.get('lifetime')
+		if ( ! lifetime ) {
+			lifetime = 12
+		}
+		if ( ! myproxy ) {
+			myproxy = MyProxyServerParams.DEFAULT_MYPROXY_SERVER
+		}
+		if ( ! port ) {
+			port = MyProxyServerParams.DEFAULT_MYPROXY_PORT
+		}
+		
+		Credential c = CredentialFactory.createFromMyProxy(username, password, myproxy, port, lifetime*3600)
+		return c
+	}
+	
+	static Credential createSlcs(ConfigObject co) {
+		
+		def idp = co.get('idp')
+		def username = co.get('username')
+		
+		println "Using user '"+username+"' at '"+idp+"'..."
+		
+		char[] pw = CliLogin
+				.askPassword("Please enter institution password")
+				
+		Credential c = CredentialFactory.createFromSlcs(null, idp, username, pw)
+		
+		return c
 	}
 	
 	static Credential loadLocal(ConfigObject co) {
